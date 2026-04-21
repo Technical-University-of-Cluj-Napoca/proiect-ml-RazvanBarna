104df1ba6a422b5ee8394619ab846baf22b47071
RazvanBarna <razvan_barna@yahoo.com>
2026-04-21 11:54:20 +0300

    shap for classification

--- Classes/Utils.py
+++ Classes/Utils.py
@@ -1,5 +1,8 @@
 from Classes import FootballFeature
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import learning_curve
 
 def create_columns(df, feat:FootballFeature, n=5):
     new_rows = []
@@ -37,3 +40,28 @@ def create_columns(df, feat:FootballFeature, n=5):
     final_df['Diff_Conceded'] = final_df['H_Conceded_Avg'] - final_df['A_Conceded_Avg']
 
     return final_df
+
+def plot_learning_curve(estimator, X, y, title):
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y,
+        cv=5,
+        n_jobs=-1,
+        train_sizes=np.linspace(0.1, 1.0, 5),
+        scoring='f1_weighted',
+    )
+    train_mean = np.mean(train_scores, axis=1)
+    train_std = np.std(train_scores, axis=1)
+    test_mean = np.mean(test_scores, axis=1)
+    test_std = np.std(test_scores, axis=1)
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(train_sizes, train_mean, 'o-', color="r", label="Scor Antrenare")
+    plt.plot(train_sizes, test_mean, 'o-', color="g", label="Scor Validare (CV)")
+
+
+    plt.title(title)
+    plt.xlabel("Nr. meciuri")
+    plt.ylabel("Scor F1-Weighted")
+    plt.legend(loc="best")
+    plt.grid(True, linestyle='--', alpha=0.7)
+    plt.show()
